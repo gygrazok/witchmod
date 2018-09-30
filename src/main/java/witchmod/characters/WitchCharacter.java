54cd06d33f8238a279805be7ff28fb1da633d2d3
Unknown <antonio.intrieri@temera.it>
2018-09-30 21:36:30 +0200

    fix for baleful ward and patch 43

--- src/main/java/witchmod/characters/WitchCharacter.java
+++ src/main/java/witchmod/characters/WitchCharacter.java
@@ -2,21 +2,18 @@ package witchmod.characters;
 
 import java.util.ArrayList;
 
-import basemod.animations.SpriterAnimation;
-import com.badlogic.gdx.math.MathUtils;
-import com.esotericsoftware.spine.AnimationState;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.EnergyManager;
 import com.megacrit.cardcrawl.core.Settings;
-import com.megacrit.cardcrawl.daily.DailyMods;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.screens.CharSelectInfo;
 import com.megacrit.cardcrawl.unlock.UnlockTracker;
 
 import basemod.BaseMod;
 import basemod.abstracts.CustomPlayer;
+import basemod.animations.SpriterAnimation;
 import witchmod.WitchMod;
 import witchmod.cardgroup.WitchCardGroup;
 import witchmod.patches.WitchEnum;
@@ -56,9 +53,6 @@ public class WitchCharacter extends CustomPlayer{
 				WitchMod.getResourcePath(WitchMod.CHAR_SHOULDER_1),
 				WitchMod.getResourcePath(WitchMod.CHAR_CORPSE), 
 				getLoadout(), 20.0F, -10.0F, 220.0F, 290.0F, new EnergyManager(ENERGY_PER_TURN));
-		if (Settings.dailyModsEnabled() && DailyMods.cardMods.get("Diverse")) {
-			masterMaxOrbs = 1;
-		}
 	}
 	
 	@Override
--- src/main/java/witchmod/powers/BalefulWardPower.java
+++ src/main/java/witchmod/powers/BalefulWardPower.java
@@ -2,7 +2,6 @@ package witchmod.powers;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
-import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -12,7 +11,7 @@ import witchmod.WitchMod;
 public class BalefulWardPower extends AbstractWitchPower {
 	public static final String POWER_ID = "BalefulWardPower";
 	public static final String NAME = "Baleful Ward";
-	public static final String[] DESCRIPTIONS = new String[]{ "If Block is broken add a Baleful Ward to your hand."};
+	public static final String[] DESCRIPTIONS = new String[]{ "If Block is broken add #b", " Baleful Ward to your hand."};
 	public static final String IMG = "powers/balefulward.png";
 	private AbstractCard card;
 	public BalefulWardPower(AbstractCard cardToCopy) {
@@ -28,21 +27,22 @@ public class BalefulWardPower extends AbstractWitchPower {
 	}
 
 	@Override
+	public void updateDescription() {
+		description = DESCRIPTIONS[0]+amount+DESCRIPTIONS[1];
+	}
+	
+	@Override
 	public void onDamageAbsorbedByBlock(int amount, int actualBlockLost, int currentBlock) {
 		if (currentBlock == 0) {
 			flash();
-			AbstractDungeon.actionManager.addToTop(new MakeTempCardInHandAction(card.makeStatEquivalentCopy()));
+			AbstractDungeon.actionManager.addToTop(new MakeTempCardInHandAction(card.makeStatEquivalentCopy(),this.amount));
 			AbstractDungeon.actionManager.addToTop(new RemoveSpecificPowerAction(owner, owner, BalefulWardPower.POWER_ID));
 		}
 	}
 
 	@Override
 	public void atEndOfRound() {
-		if (amount == 0) {
-			AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction(owner, owner, BalefulWardPower.POWER_ID));
-		} else {
-			AbstractDungeon.actionManager.addToBottom(new ReducePowerAction(owner, owner, BalefulWardPower.POWER_ID, 1));
-		}
+		AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction(owner, owner, BalefulWardPower.POWER_ID));
 	}
 
 
